a242579faa98128a6995bb187ea3495e0516dd75
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2023-02-28 22:05:43 +0900

    パラメーター調整

--- src/main/java/com/example/mosaic/MainActivity.java
+++ src/main/java/com/example/mosaic/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.graphics.Bitmap;
 import android.graphics.Point;
-import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +16,8 @@ import android.widget.Toast;
 import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.ortiz.touchview.TouchImageView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
@@ -37,11 +38,10 @@ public class MainActivity extends AppCompatActivity
 			}
 		}
 	};
-	private final Point point = new Point();
 	private TextView mosaicLevelText;
 	private SeekBar mosaicLevel;
 	private Uri imageUri;
-	private CustomTouchImageView selectedImage;
+	private TouchImageView selectedImage;
 	private PreviewRect previewImage;
 
 	// アクセシビリティワーニングを黙らせる
@@ -76,19 +76,19 @@ public class MainActivity extends AppCompatActivity
 			public void onStopTrackingTouch( SeekBar seekBar ) {}
 		} );
 
-		// タッチされた場所を通知するTouchImageViewの拡張クラスにあるインターフェース
-		selectedImage.setTouchPointLister( () -> this.point );
-
 		// 画像内タッチイベント
 		selectedImage.setOnTouchListener( ( v, event ) ->
 		{
 			v.performClick();
-			var size = getDisplaySize( this );
+			var displaySize = getDisplaySize( this );
+			var imgSize = selectedImage.getDrawable().getBounds();
 
-			// TODO 画面サイズー＞画像サイズに対応するメソッドを作成する
-			// 範囲外に出ないように調整
-			int x = Utility.clamp( 0, ( int ) event.getX(), size.x );
-			int y = Utility.clamp( 0, ( int ) event.getY(), size.y );
+			float tx = Math.max( ( float ) imgSize.right / displaySize.x, 1.f );
+			float ty = Math.max( ( float ) imgSize.bottom / displaySize.y, 1.f );
+
+			// 画像外に出ないように調整
+			int x = Utility.clamp( 0, ( int ) ( event.getX() * tx - 150 ), imgSize.width() - PreviewRect.CROP_SIZE - 1 );
+			int y = Utility.clamp( 0, ( int ) ( event.getY() * ty - 150 ), imgSize.height() - PreviewRect.CROP_SIZE - 1 );
 
 			setPreview( x, y );
 
@@ -103,14 +103,6 @@ public class MainActivity extends AppCompatActivity
 				imageUri = uri;
 				selectedImage.setImageURI( imageUri );
 				setPreview( 0, 0 );
-
-				// Debug
-				{
-					var d = ( ( BitmapDrawable ) selectedImage.getDrawable() ).getBitmap();
-					Log.d( "AppDebug", String.format( "Image   ( h, w ) = ( %d, %d )", d.getHeight(), d.getWidth() ) );
-					var dsp = getDisplaySize( this );
-					Log.d( "AppDebug", String.format( "Display ( x, y ) = ( %d, %d )", dsp.x, dsp.y ) );
-				}
 			}
 			else {
 				// 画像が選択されなければアプリを終了させる
@@ -164,8 +156,6 @@ public class MainActivity extends AppCompatActivity
 
 	private void setPreview( int x, int y )
 	{
-		this.point.set( x, y );
-
 		// プレビュー画面に画像をセット
 		previewImage.setImageBitmap(
 				Bitmap.createBitmap( ( ( BitmapDrawable ) selectedImage.getDrawable() ).getBitmap(), x, y, PreviewRect.CROP_SIZE, PreviewRect.CROP_SIZE )
