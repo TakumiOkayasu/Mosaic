9fd1327e96df58c4672667e7d0a0fc517f0ae6a0
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2023-03-01 21:15:50 +0900

    Switchをつけ忘れていたのでつけた

--- src/main/java/com/example/mosaic/MainActivity.java
+++ src/main/java/com/example/mosaic/MainActivity.java
@@ -9,7 +9,9 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,14 +19,13 @@ import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.ortiz.touchview.TouchImageView;
-
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity
 {
+	private static final String LOG_TAG = "AppDebug";
 	private final BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback( this )
 	{
 		@Override
@@ -41,8 +42,10 @@ public class MainActivity extends AppCompatActivity
 	private TextView mosaicLevelText;
 	private SeekBar mosaicLevel;
 	private Uri imageUri;
-	private TouchImageView selectedImage;
+	private ImageView selectedImage;
 	private PreviewRect previewImage;
+	@SuppressLint( "UseSwitchCompatOrMaterialCode" )
+	private Switch mosaicMode;
 
 	// アクセシビリティワーニングを黙らせる
 	@SuppressLint( "ClickableViewAccessibility" )
@@ -54,8 +57,20 @@ public class MainActivity extends AppCompatActivity
 
 		mosaicLevelText = findViewById( R.id.tv_mosaic_level );
 		mosaicLevel = findViewById( R.id.sb_mosaic_level );
-		selectedImage = findViewById( R.id.tiv_selected_img );
+		selectedImage = findViewById( R.id.iv_selected_img );
 		previewImage = findViewById( R.id.pr_preview );
+		mosaicMode = findViewById( R.id.sw_mosaic );
+
+		mosaicMode.setOnClickListener( v ->
+				mosaicMode.setText( mosaicMode.isChecked() ? "モザイクかけるモード" : "みるだけモード" )
+		);
+
+		mosaicMode.setOnCheckedChangeListener( ( buttonView, isChecked ) -> {
+			if( isChecked ) {
+				// ここにモザイクをかける処理を書く
+				Log.d( LOG_TAG, "Switch Changed!" );
+			}
+		} );
 
 		// 最初の一回はTextViewが表示されないので呼んであげる
 		showMosaicLevel();
@@ -76,6 +91,7 @@ public class MainActivity extends AppCompatActivity
 			public void onStopTrackingTouch( SeekBar seekBar ) {}
 		} );
 
+
 		// 画像内タッチイベント
 		selectedImage.setOnTouchListener( ( v, event ) ->
 		{
@@ -83,14 +99,18 @@ public class MainActivity extends AppCompatActivity
 			var displaySize = getDisplaySize( this );
 			var imgSize = selectedImage.getDrawable().getBounds();
 
-			float tx = Math.max( ( float ) imgSize.right / displaySize.x, 1.f );
-			float ty = Math.max( ( float ) imgSize.bottom / displaySize.y, 1.f );
+			Log.d( LOG_TAG, String.format( "ImgRect[%d, %d][%d, %d]", imgSize.top, imgSize.left, imgSize.bottom, imgSize.right ) );
+
+			int tx = ( int ) ( event.getX() * Math.max( ( float ) imgSize.right / displaySize.x, 1.f ) ) - 130;
+			int ty = ( int ) ( event.getY() * Math.max( ( float ) imgSize.bottom / displaySize.y, 1.f ) );
+
+			Log.d( LOG_TAG, String.format( "tx, ty[%d, %d]", tx, ty ) );
 
 			// 画像外に出ないように調整
-			int x = Utility.clamp( 0, ( int ) ( event.getX() * tx - 150 ), imgSize.width() - PreviewRect.CROP_SIZE - 1 );
-			int y = Utility.clamp( 0, ( int ) ( event.getY() * ty - 150 ), imgSize.height() - PreviewRect.CROP_SIZE - 1 );
+			int x = Utility.clamp( 0, tx, imgSize.width() - PreviewRect.CROP_SIZE - 1 ) + 50;
+			int y = Utility.clamp( 0, ty, imgSize.height() - PreviewRect.CROP_SIZE - 1 ) + 300;
 
-			setPreview( x, y );
+			// setPreview( x, y );
 
 			return true;
 		} );
@@ -102,7 +122,7 @@ public class MainActivity extends AppCompatActivity
 			if( uri != null ) {
 				imageUri = uri;
 				selectedImage.setImageURI( imageUri );
-				setPreview( 0, 0 );
+				//setPreview( 0, 0 );
 			}
 			else {
 				// 画像が選択されなければアプリを終了させる
@@ -154,11 +174,12 @@ public class MainActivity extends AppCompatActivity
 		return new Point( w, h );
 	}
 
-	private void setPreview( int x, int y )
+	// 中心座標をもらい、描画する
+	private void setPreview( Point center )
 	{
 		// プレビュー画面に画像をセット
 		previewImage.setImageBitmap(
-				Bitmap.createBitmap( ( ( BitmapDrawable ) selectedImage.getDrawable() ).getBitmap(), x, y, PreviewRect.CROP_SIZE, PreviewRect.CROP_SIZE )
+				Bitmap.createBitmap( ( ( BitmapDrawable ) selectedImage.getDrawable() ).getBitmap(), center.x, center.y, PreviewRect.CROP_SIZE, PreviewRect.CROP_SIZE )
 		);
 	}
 }
