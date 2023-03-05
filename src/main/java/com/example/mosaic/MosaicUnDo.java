fe949cce9c0699bfcbc452252eac901297f18d60
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2023-03-05 15:58:08 +0900

    Memento Patternの枠組みだけ作成

--- src/main/java/com/example/mosaic/MosaicUnDo.java
+++ src/main/java/com/example/mosaic/MosaicUnDo.java
@@ -1,11 +1,28 @@
 package com.example.mosaic;
 
+import android.util.Log;
 import android.view.View;
+
 
 public class MosaicUnDo implements View.OnClickListener
 {
 	@Override
 	public void onClick( View v )
+	{
+	}
+
+	public class Memento
+	{
+		private Memento()
+		{
+		}
+	}
+	private Memento createMemento()
+	{
+		return null;
+	}
+
+	private void restoreMemento( Memento memento )
 	{
 
 	}
